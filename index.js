f0721f953be8d24ed3dc29688a08d69b7591a026
bradeac <bradeacamil@gmail.com>
2019-05-09 22:12:02 +0300

    First working version

---
+++ index.js
@@ -0,0 +1,30 @@
+const NoSleep = require('nosleep.js')
+const noSleep = new NoSleep()
+
+let clicked = false
+
+const button = document.getElementsByTagName('button')[0]
+const paragraph = document.getElementsByTagName('p')[0]
+
+document.getElementById('toggle').addEventListener('click', function () {
+    if (!clicked) {
+        clicked = !clicked
+
+        button.innerHTML = 'Deactivate keep screen on'
+        paragraph.innerHTML = 'Keep screen on status: activated !'
+
+        noSleep.enable()
+
+        console.log("Activated", clicked)
+    } else {
+        clicked = !clicked
+
+        button.innerHTML = 'Activate keep screen on'
+        paragraph.innerHTML = 'Keep screen on status: deactivated'
+
+        noSleep.disable()
+
+        console.log('Deactivated', clicked)
+    }
+
+})
